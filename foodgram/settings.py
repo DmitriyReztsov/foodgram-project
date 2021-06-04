7a7a9e146036c5d0dfc51819aac3503930f1255e
Dmitriy Reztsov <68148319+DmitriyReztsov@users.noreply.github.com>
2021-06-04 21:23:53 +0300

    prepare to dock

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -66,8 +66,19 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
     }
 }'''
 
-DATABASES = {
+'''DATABASES = {
     'default': env.db(),
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
